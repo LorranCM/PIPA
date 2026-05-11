7db19735e12c93f571d62d464f186a38f7b3a94e
Lorran <Lorrancalazans@gmail.com>
2026-05-11 12:03:37 -0300

    feat: add update calendar data page

---
+++ tests/update_calendar_data.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        require __DIR__. "/../components/preset.php"; 
+        require __DIR__. "/../components/updates/get_calendar_data.php"; 
+    ?>
+</body>
+</html>
